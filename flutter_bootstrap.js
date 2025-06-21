7751e688b347994cf2eaba4d6966997ea9b00003
Pho Tran <tranthanhpho2011@gmail.com>
2025-06-21 21:53:48 +0700

    Release 9.2.3+923

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1802051583"
+    serviceWorkerVersion: "2917025647"
   }
 });
